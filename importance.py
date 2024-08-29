6903dd8ddfbf3960a02465edcd57e03ac3aba0d7
root <jabberwockyang@163.com>
2024-08-29 14:55:09 +0800

    initial commit

---
+++ importance.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import json
+import os
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+from best_params import opendb, get_best_params
+
+# todo 去除离群值
+
+def within_n_std(data, column, n):
+    return (data[column] - data[column].median()).abs() <= n * data[column].std()
+
+# list all folder in repodir 
+def LoadData(repodir):
+        
+    allfolder = [dir for dir in os.listdir(repodir) if os.path.isdir(os.path.join(repodir, dir))]
+    dflist = [] 
+    for folder in allfolder:
+        labels_trained = [dir for dir in os.listdir(os.path.join(repodir, folder)) if os.path.isdir(os.path.join(repodir, folder, dir))]
+        for label in labels_trained:
+            df_path = os.path.join(repodir, folder, label, "result","0",'feature_importance.csv')
+            df = pd.read_csv(df_path)
+            df['group'] = label
+            df['param_id'] = folder
+            dflist.append(df)
+
+    df = pd.concat(dflist, axis=0)
+    return df, labels_trained
+
+# plot a verticle boxplot with dots x is Importance y is Feature for each group
+
+def LoadDataFromNNI(nnidir, metric_to_optimize: list=[('default','minimize')], number_of_trials=25):
+    best_db_path = os.path.join(nnidir, 'db','nni.sqlite')
+    df = opendb(best_db_path)
+    ls_of_params = get_best_params(df, metric_to_optimize, number_of_trials)
+    
+    dflist = []
+    for param_id, _, sequence_ids in ls_of_params:
+        for sequence_id in sequence_ids:
+            print(sequence_id)
+            bestparamfolder = os.path.join(nnidir, 'result', str(sequence_id))
+            dfpath = os.path.join(bestparamfolder, 'feature_importance.csv')
+            df = pd.read_csv(dfpath)
+            df['sequence_ids'] = sequence_id
+            df['param_id'] = param_id
+            df['group'] = 'all'
+            dflist.append(df)
+    df = pd.concat(dflist, axis=0)
+    return df, ['all']
+
+def PlotImportance(df, labels, outdir,n=15):
+    # rename column featuer to Feature if feature in df.columns
+    if 'feature' in df.columns:
+        df.rename(columns={'feature': 'Feature'}, inplace=True)
+    if 'weight' in df.columns:
+        df.rename(columns={'weight': 'Importance'}, inplace=True)
+
+    mapcategory2feature = {}    
+    unique_feature = df['Feature'].unique().tolist()
+    for key in ExaminationItemClass.keys():
+        namelist = [x[1] for x in ExaminationItemClass[key]]
+        pattern = '|'.join(namelist)
+        mapcategory2feature[key] = [x for x in unique_feature if re.search(pattern, x)]
+    mapcategory2feature['Clinical'] = ['FirstVisitAge','CIndU','Gender']
+    mapcategory2feature['DerivativeVariables'] = [x for x in unique_feature if '_div_' in x]
+
+    df['Feature_group'] = df['Feature'].map({v: k for k in mapcategory2feature for v in mapcategory2feature[k]})
+    
+    # map categoty to color
+    colors = sns.color_palette("colorblind", len(mapcategory2feature.keys()))
+    mapcategory2color = dict(zip(mapcategory2feature.keys(), colors))
+    top_n_list = []
+    for label in labels:
+        print(label)    
+        data = df[df['group'] == label]
+        #  for each feature remove outliers by mean +- n*std
+        filtereddata = []
+        for feature in data['Feature'].unique():
+            temp = data[data['Feature'] == feature]
+            temp.reset_index(drop=True, inplace=True)
+            filtereddata.append(temp[within_n_std(temp, 'Importance', 1)])
+        data = pd.concat(filtereddata, axis=0, ignore_index=True)
+        # importance mean topn
+        feature_to_plot = data.groupby('Feature')['Importance'].median().sort_values(ascending=False).index[:n].tolist()
+        
+        top_n_list.append({'label': label, 
+                           'n': n,
+                           'top_n': feature_to_plot})
+        data = data[data['Feature'].isin(feature_to_plot)]   
+        data['Feature'] = pd.Categorical(data['Feature'], categories=feature_to_plot, ordered=True)
+        data = data.sort_values('Feature')
+
+        # 创建一个图形和轴对象
+        fig, ax = plt.subplots(figsize=(8, 8)) # figsize=(width, height) in inches
+
+        # 设置轴对象的位置和大小
+        ax.set_position([0.5, 0.1, 0.45, 0.8])  # [left, bottom, width, height] in figure coordinates
+
+        # 自定义中位线和误差线
+        # boxprops = dict(linestyle='-', linewidth=2, color='blue')  # 盒子的样式
+        medianprops = dict(linestyle='-', linewidth=3, color='black')  # 中位数线的样式
+        whiskerprops = dict(linestyle='--', linewidth=2, color='black')  # 误差线的样式
+        # boxplot with dots
+        sns.boxplot(data , x = 'Importance', y = 'Feature', 
+                    hue='Feature_group',  
+                    palette=mapcategory2color,
+                    ax = ax,
+                    width = 0.5, 
+                    # boxprops=boxprops, 
+                    medianprops=medianprops, whiskerprops=whiskerprops)
+        sns.stripplot(data, x = 'Importance', y = 'Feature', color='orange',
+                    size=5, jitter=0.25, ax = ax, marker='o', 
+                    )
+
+
+        plt.title('Feature Importance in ' + label, fontsize=20,pad=15)
+        plt.xlabel('Importance', fontsize=15)
+        plt.xlim(0, max(data['Importance']) + 10)
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize=15)
+        plt.ylabel('Feature', fontsize=15)
+        plt.savefig(os.path.join(outdir, label + '_feature_importance.png'))
+        plt.close()
+
+    return top_n_list
+
+def argparser():
+    import argparse
+    parser = argparse.ArgumentParser(description='Plot feature importance')
+    parser.add_argument('--repodir', type=str, default = None, help='repo directory')
+    parser.add_argument('--nnidir', type=str, default = None, help='nni directory')
+    parser.add_argument('--metric', type=str, default = None, help='metric to optimize')
+    parser.add_argument('--minimize', type=bool, default = None, help='minimize or maximize')
+    parser.add_argument('--number_of_trials', type=int, default = None, help='number of trials')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    with open ('ExaminationItemClass_ID.json', 'r') as json_file:
+        ExaminationItemClass = json.load(json_file)
+    args = argparser()
+    repodir = args.repodir
+    nnidir = args.nnidir
+    
+
+    # RefreshFeatureImportance(repodir)
+    if repodir:
+        outdir = os.path.join('VariablesImportance', os.path.basename(repodir))
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        df, labels = LoadData(repodir)
+    elif nnidir:
+        nnidir = os.path.realpath(nnidir) 
+        outdir = os.path.join('VariablesImportance', f"{os.path.basename(nnidir)}_{args.metric}_top{args.number_of_trials}_fromnni")
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        m = 'minimize' if args.minimize else 'maximize'
+        df, labels = LoadDataFromNNI(nnidir, metric_to_optimize=[(args.metric, m)], number_of_trials=args.number_of_trials)
+
+    top_n_list = PlotImportance(df, labels, outdir)
+    df.to_csv(os.path.join(outdir, 'feature_importance_summary.csv'), index=False)
+    with open(os.path.join(outdir, 'feature_importance_summary.json'), 'w') as f:
+        json.dump(top_n_list, f, indent=4)
+        
---
+++ utils.py
@@ -0,0 +1,328 @@
+import warnings
+import xgboost as xgb
+import pandas as pd
+from sklearn.metrics import mean_squared_error, roc_curve, auc, precision_recall_curve
+import os
+from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from loguru import logger
+import itertools 
+
+# 数据加载
+def load_data(filepath):
+    data = pd.read_csv(filepath)
+    return data
+
+def convert_floats(o):
+    if isinstance(o, np.float32):
+        return float(o)
+    elif isinstance(o, np.ndarray):
+        return o.tolist()  # Convert ndarray to list
+    elif isinstance(o, dict):
+        return {k: convert_floats(v) for k, v in o.items()}
+    elif isinstance(o, list):
+        return [convert_floats(v) for v in o]
+    return o
+
+def LoadFeatures(filepath):
+    if filepath is None:
+        return None
+    with open(filepath, 'r') as f:
+        importancedata = json.load(f)
+    alldata = [item for item in importancedata if item['label'] == 'all'][0]
+    return alldata['top_n']
+
+# 数据预处理
+def preprocess_data(df, target_column, 
+                    scale_factor,log_transform, 
+                    groupingparams: Dict[str, List[str]],
+                    pick_key = '0-2', 
+                    feature_derivation = None):
+    ## filtering data
+    # remove rows with missing values in all result columns
+    with open ('ExaminationItemClass_ID.json', 'r') as json_file:
+        ExaminationItemClass = json.load(json_file)
+    namelist = [x[1] for x in ExaminationItemClass['CommonBloodTest']]
+    result_cols2 = df.columns[df.columns.str.contains("|".join(namelist))]
+    df = df.dropna(subset=result_cols2, how='all') 
+    # remove rows with missing values in target column
+    df = df.dropna(subset=[target_column])
+    # remove columns in results_col with only 1 unique value
+    result_cols = df.columns[df.columns.str.contains('Avg|Count|Sum|Max|Min|Median|Std|Skew|Kurt|Pct')]
+    df = df.drop(columns=result_cols[df[result_cols].nunique() == 1])
+
+    # Group data by visitdurationgroup and calculate weights
+    df['visitdurationgroup'] = pd.cut(df['VisitDuration'], 
+                                      bins=[0, 42, 365, 1095, 1825,10000], 
+                                      labels=["<6w", "6w-1y", "1-3y", "3-5y", "5y+"], ordered=True)
+    df = df.dropna(subset=['visitdurationgroup'])
+    weights = df['visitdurationgroup'].value_counts(normalize=True)
+    df['sample_weight'] = df['visitdurationgroup'].map(lambda x: 1 / (weights[x] + 1e-10))
+
+    # group data by agegroup
+    bins = groupingparams['bins']
+    labels = groupingparams['labels']
+
+    df = df.assign(agegroup=pd.cut(df['FirstVisitAge'], bins= bins, labels= labels))
+    print(df['agegroup'].value_counts())
+    df['agegroup'] = pd.Categorical(df['agegroup'], categories= labels, ordered=True)
+
+    # Missing value imputation and other preprocessing
+    warnings.filterwarnings("ignore", category=RuntimeWarning, message="Mean of empty slice")
+    result_cols = df.columns[df.columns.str.contains('Avg|Count|Sum|Max|Min|Median|Std|Skew|Kurt|Pct')]
+    overall_median = df[result_cols].median()
+    df[result_cols] = df.groupby('agegroup', observed=True)[result_cols].transform(
+        lambda x: x.fillna(x.median() if not np.isnan(x.median()) else overall_median)
+    )
+    warnings.filterwarnings("default", category=RuntimeWarning, message="Mean of empty slice")
+
+    # subsetting data
+    if pick_key != 'all':
+        df = df[df['agegroup'] == pick_key]
+    else:
+        pass
+    # scale the data by min-max 
+    for col in result_cols:
+        df[col] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
+
+    # Feature derivation
+    if isinstance(feature_derivation, list):
+        combinations = list(itertools.combinations(feature_derivation, 2))
+        # 对于每一对特征组合，生成派生变量并添加到df中
+        for (feat1, feat2) in combinations:
+            # 创建新的派生变量列名
+            new_col_name = f'{feat1}_div_{feat2}'
+            df[new_col_name] = df[feat1] / (df[feat2] + 1e-10)
+
+    print(df.head())
+    # Generate data for training
+    X = df.drop(columns=[target_column, 'sample_weight','agegroup', 'visitdurationgroup'])
+    y = df[target_column]
+    # scaled by scale_factor linearly
+    y = np.round(y / scale_factor) + 1
+    # log transform
+    if log_transform == "log2":
+        y = np.log2(y)
+    elif log_transform == "log10":
+        y = np.log10(y)
+    else:
+        y = y
+    # get sample_weight
+    sample_weight = df['sample_weight']
+    logger.info(f"Data for 'agegroup' {pick_key} is ready, X shape: {X.shape}, y shape: {y.shape}")
+    return X, y, sample_weight
+
+
+def plot_roc_curve(fpr, tpr, auc, savepath):
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label=f'roc curve (area = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.savefig(savepath)
+
+def plot_prc_curve(precision, recall, auc, savepath):
+    plt.figure()
+    lw = 2
+    plt.plot(recall, precision, color='darkorange', lw=lw, label=f'prc curve (area = %0.2f)' % auc)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall curve example')
+    plt.legend(loc="lower right")
+    plt.savefig(savepath)
+
+def plot_feature_importance(model, savepath):
+    dflist = [] 
+    for target in ['weight', 'total_gain', 'total_cover', 'gain', 'cover']:
+        importance = model.get_score(importance_type= target)
+        importance_df = pd.DataFrame(index=importance.keys(), data={target: list(importance.values())})
+
+        dflist.append(importance_df)
+    importance_df = pd.concat(dflist, axis=1)
+    importance_df['feature'] = importance_df.index  
+    importance_df.to_csv(os.path.join(savepath, 'feature_importance.csv'), index=False)
+
+    importance_df = importance_df.sort_values('weight', ascending=False)
+    #scale to 0-1
+    for col in ['weight', 'total_gain', 'total_cover', 'gain', 'cover']:
+        importance_df[col] = importance_df[col] / importance_df[col].max()
+    # plot first 25 features
+    importance_df = importance_df.head(25)
+    importance_df = importance_df.loc[:, ['feature', 'weight', 'total_gain', 'total_cover']]
+    plt.figure()
+    fig, ax = plt.subplots(figsize=(20, 10)) # figsize=(width, height) in inches
+    ax.set_position([0.1, 0.3, 0.8, 0.6])  # [left, bottom, width, height] in figure coordinates
+
+    importance_df.plot(kind='bar', ax = ax)
+    plt.xlabel('Feature')
+    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('Importance')
+    plt.title('Feature Importance')
+    plt.savefig(os.path.join(savepath, 'feature_importance.png'))
+
+
+# 自定义评估函数1
+def prerec_auc_metric(y_test, y_pred):
+    auc_list = []
+    for binary_threshold in [42]:
+        y_test_binary = np.where(y_test.copy() > binary_threshold, 1, 0)
+        prec, rec, thresholds = precision_recall_curve(y_test_binary, y_pred) 
+        prerec_auc = auc(rec, prec)
+        auc_list.append(
+            {"binary_threshold": binary_threshold,
+            "precision": prec,
+            "recall": rec,
+            "thresholds": thresholds,
+            "prerec_auc": prerec_auc})
+    return auc_list
+
+# 自定义评估函数2
+def roc_auc_metric(y_test, y_pred):
+    auc_list = []
+    for binary_threshold in [42]:
+        y_test_binary = np.where(y_test.copy() > binary_threshold, 1, 0)
+        fpr, tpr, thresholds = roc_curve(y_test_binary, y_pred)
+        roc_auc = auc(fpr, tpr)
+        auc_list.append(
+            {"binary_threshold": binary_threshold,
+            "fpr": fpr,
+            "tpr": tpr,
+            "thresholds": thresholds,
+            "roc_auc": roc_auc})
+    return auc_list
+
+
+# 模型评估
+def evaluate_model(model, dtest, result_dir,scale_factor, log_transform):
+    y_test = dtest.get_label()
+    y_pred = model.predict(dtest)
+
+    if log_transform == "log2":
+        y_test_reversed = 2 ** y_test
+        y_pred_reversed = 2 ** y_pred
+    elif log_transform == "log10":
+        y_test_reversed = 10 ** y_test
+        y_pred_reversed = 10 ** y_pred
+    else:
+        y_test_reversed = y_test
+        y_pred_reversed = y_pred
+    
+    # reverse scale_factor
+    y_test_reversed = (y_test_reversed - 1) * scale_factor
+    y_pred_reversed = (y_pred_reversed - 1) * scale_factor
+
+    # loss
+    loss = mean_squared_error(y_test_reversed, y_pred_reversed)
+
+    # roc auc
+    roc_auc_json = roc_auc_metric(y_test_reversed, y_pred_reversed)
+    max_roc_auc = max([roc_obj["roc_auc"] for roc_obj in roc_auc_json])
+
+    # prerec auc
+    prerec_auc_json = prerec_auc_metric(y_test_reversed, y_pred_reversed)
+    max_prerec_auc = max([prerec_obj["prerec_auc"] for prerec_obj in prerec_auc_json])
+
+    # save result
+    auc_results = {
+        "roc_auc": roc_auc_json,
+        "prerec_auc": prerec_auc_json
+    }
+    with open(os.path.join(result_dir, 'auc_results.json'), 'w') as f:
+        json.dump(convert_floats(auc_results), f, ensure_ascii=False, indent=4)
+
+    # save plot
+    for item in roc_auc_json:
+        binary_threshold = item["binary_threshold"]
+        fpr = item["fpr"]
+        tpr = item["tpr"]
+        roc_auc = item["roc_auc"]
+
+        save_path = os.path.join(result_dir, f'{binary_threshold}_roc.png')
+        plot_roc_curve(fpr, tpr, roc_auc, savepath=save_path)
+
+    for item in prerec_auc_json:
+        binary_threshold = item["binary_threshold"]
+        prec = item["precision"]
+        rec = item["recall"]
+        prerec_auc = item["prerec_auc"]
+
+        save_path = os.path.join(result_dir, f'{binary_threshold}_prc.png')
+        plot_prc_curve(rec, prec, prerec_auc, savepath=save_path)
+
+    return loss, max_roc_auc, max_prerec_auc
+
+# 保存模型checkpoint
+def save_checkpoint(model, checkpoint_path):
+    # Create directory if not exists
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    # Save
+    model.save_model(checkpoint_path)
+    logger.info(f"Checkpoint saved to {checkpoint_path}")
+
+def custom_eval_roc_auc_factory(custom_metric_key, scale_factor, log_transform):
+    if custom_metric_key == 'prerec_auc':
+        # 用于传入 train_model 的自定义评估函数1
+        def custom_eval_prerec_auc(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
+            '''
+            In the above code snippet, squared_log is the objective function we want. 
+            It accepts a numpy array predt as model prediction, and the training DMatrix for obtaining required information, including labels and weights (not used here). 
+            This objective is then used as a callback function for XGBoost during training by passing it as an argument to xgb.train:
+            '''
+            y_train = dtrain.get_label()
+            y_pred = predt
+            if log_transform == "log2":
+                y_train_reversed = 2 ** y_train
+                y_pred_reversed = 2 ** y_pred
+            elif log_transform == "log10":
+                y_train_reversed = 10 ** y_train
+                y_pred_reversed = 10 ** y_pred
+            else:
+                y_train_reversed = y_train
+                y_pred_reversed = y_pred
+            # reverse scale_factor
+            y_train_reversed = (y_train_reversed - 1) * scale_factor
+
+            auc_json = prerec_auc_metric(y_train_reversed, y_pred_reversed)
+            max_prerec_auc = max([auc_obj['prerec_auc'] for auc_obj in auc_json])
+            return 'prerec_auc', max_prerec_auc
+        return custom_eval_prerec_auc
+    elif custom_metric_key == 'roc_auc':
+        # 用于传入 train_model 的自定义评估函数2
+        def custom_eval_roc_auc(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
+            '''
+            In the above code snippet, squared_log is the objective function we want. 
+            It accepts a numpy array predt as model prediction, and the training DMatrix for obtaining required information, including labels and weights (not used here). 
+            This objective is then used as a callback function for XGBoost during training by passing it as an argument to xgb.train:
+            '''
+            y_train = dtrain.get_label()
+            y_pred = predt
+            if log_transform == "log2":
+                y_train_reversed = 2 ** y_train
+                y_pred_reversed = 2 ** y_pred
+            elif log_transform == "log10":
+                y_train_reversed = 10 ** y_train
+                y_pred_reversed = 10 ** y_pred
+            else:
+                y_train_reversed = y_train
+                y_pred_reversed = y_pred
+            # reverse scale_factor
+            y_train_reversed = (y_train_reversed - 1) * scale_factor
+            auc_json = roc_auc_metric(y_train_reversed, y_pred_reversed)
+            max_roc_auc = max(auc_obj['roc_auc'] for auc_obj in auc_json)
+            return 'roc_auc', max_roc_auc
+        return custom_eval_roc_auc
+    
+    elif  custom_metric_key == None:
+        return None
+    
